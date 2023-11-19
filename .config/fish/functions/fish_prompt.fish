b3393f921985e5714b9118a4bb614112bcf5b529
Ben Burkert <ben@benburkert.com>
2023-11-19 13:27:19 -0500

    fish: add basic prompt

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,110 @@
+function fish_prompt
+    # This prompt shows:
+    # - the current path (with prompt_pwd)
+    # - the current git status, if any, with custom git query
+    # - date +%X
+    # - current background jobs, if any
+
+    set -l laststatus $status
+    set -l retc red
+    test $laststatus = 0; and set retc green
+
+    set -l git_info
+    if set -l git_branch (command git symbolic-ref HEAD 2>/dev/null | string replace refs/heads/ '')
+        set git_branch (set_color -o blue)"$git_branch"
+        set -l git_status
+        if not command git diff-index --quiet HEAD --
+            if set -l count (command git rev-list --count --left-right $upstream...HEAD 2>/dev/null)
+                echo $count | read -l ahead behind
+                if test "$ahead" -gt 0
+                    set git_status "$git_status"(set_color red)⬆
+                end
+                if test "$behind" -gt 0
+                    set git_status "$git_status"(set_color red)⬇
+                end
+            end
+            for i in (git status --porcelain | string sub -l 2 | sort | uniq)
+                switch $i
+                    case "."
+                        set git_status "$git_status"(set_color green)✚
+                    case " D"
+                        set git_status "$git_status"(set_color red)✖
+                    case "*M*"
+                        set git_status "$git_status"(set_color green)✱
+                    case "*R*"
+                        set git_status "$git_status"(set_color purple)➜
+                    case "*U*"
+                        set git_status "$git_status"(set_color brown)═
+                    case "??"
+                        set git_status "$git_status"(set_color red)≠
+                end
+            end
+        else
+            set git_status (set_color green):
+        end
+        set git_info " $git_status$git_branch"
+    end
+
+    set -q __fish_git_prompt_showupstream
+    or set -g __fish_git_prompt_showupstream auto
+
+    function _prompt_wrapper
+        set clr $argv[1]
+        set -l field_name $argv[2]
+        set -l field_value $argv[3]
+
+        set_color normal
+        set_color white
+        echo -n '❙'
+        test -n "$field_name"
+        and echo -n "$field_name "
+        set_color $clr
+        echo -n $field_value
+        set_color -o white
+    end
+
+    set_color normal
+    set_color white
+    echo -n '┬─'
+    echo -n '❰'
+
+    # PWD
+
+    set_color yellow
+    echo -n (prompt_pwd)
+
+    # git
+
+    test -n "$git_info"
+    and _prompt_wrapper yellow '' $git_info
+
+    # date
+
+    _prompt_wrapper green "" (date +%X)
+    
+    # status
+
+    _prompt_wrapper $retc "" $laststatus
+
+    # EOL
+    set_color white
+    echo -n '❱'
+    echo
+
+    # Background jobs
+    set_color normal
+
+    for job in (jobs)
+        set_color white
+        echo -n '│ '
+        set_color brown
+        echo $job
+    end
+
+    set_color normal
+    set_color white
+    echo -n '╰─>'
+    set_color -o red
+    echo -n '$ '
+    set_color normal
+end
