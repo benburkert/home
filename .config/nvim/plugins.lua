032a085d461eb627639931d5e050b00702fdd67e
Ben Burkert <ben@benburkert.com>
2023-11-28 20:49:09 -0500

    nvim: add lualine

--- .config/nvim/plugins.lua
+++ .config/nvim/plugins.lua
@@ -12,16 +12,19 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  use { 'wbthomason/packer.nvim' }
   use { 'ellisonleao/gruvbox.nvim' }
   use { 'nathom/filetype.nvim' }
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-
+  use {
+	  'nvim-lualine/lualine.nvim',
+	  requires = { { 'nvim-tree/nvim-web-devicons' } }
+  }
   use {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.4',
 	requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'wbthomason/packer.nvim' }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
