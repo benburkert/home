520d556df9da1457424f9410bab3b5d1d2dec874
Ben Burkert <ben@benburkert.com>
2025-08-28 18:33:20 -0600

    fish: suppress greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,9 @@
+set fish_greeting
+
 switch (uname)
     case Darwin
         fish_add_path /opt/homebrew/bin
 end
-
-
 
 if status is-interactive
     alias home "git --work-tree=$HOME --git-dir=$HOME/.home"
