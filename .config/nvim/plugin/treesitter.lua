1149263123fcf4ba121bbf6712faa88a2ab4dd9f
Ben Burkert <ben@benburkert.com>
2025-09-26 23:15:35 -0400

    nvim: double entry for zig

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -7,7 +7,7 @@ return {
     config = function ()
       require('nvim-treesitter.configs').setup({
         ensure_installed = {
-          'c', 'cpp', 'css', 'json', 'nix', 'regex', 'rust', 'sql', 'toml', 'vimdoc', 'yaml', 'zig',
+          'c', 'cpp', 'css', 'json', 'nix', 'regex', 'rust', 'sql', 'toml', 'vimdoc', 'yaml',
 
           -- git
           'diff', 'git_config', 'git_rebase', 'gitattributes', 'gitcommit',
